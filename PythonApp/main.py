5a7945e3e809f99befcf69494c405f8c47d9475c
NicaZadirieva <no.demchenko@yandex.ru>
2025-12-22 19:43:36 +0300

    feat: демо дефолтное значение

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -2,7 +2,7 @@ class Note:
     title: str
     description: str
 
-    def __init__(self, title: str, description: str):
+    def __init__(self, title: str, description: str = ""):
         self.title = title
         self.description = description
         print("Создана заметка")
@@ -10,3 +10,7 @@ class Note:
 note = Note("Это title", "Это description")
 print(note.title)
 print(note.description)
+
+noteOutDesc = Note("Это title для noteOutDesc")
+print(noteOutDesc.title)
+print(noteOutDesc.description)
