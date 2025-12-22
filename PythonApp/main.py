87d74df269b5a3436c2ea02a655c1ef42c907b85
NicaZadirieva <no.demchenko@yandex.ru>
2025-12-22 19:39:12 +0300

    feat: демо __init__

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,21 +1,12 @@
-class Task:
-    """Задача"""
-    done: bool = False
+class Note:
     title: str
+    description: str
 
-    def get_info(self):
-        """Получение данных задачи"""
-        print("Задача")
-    
-    def set_info(self, text: str):
-        """Установка title"""
-        self.title = text
+    def __init__(self, title: str, description: str):
+        self.title = title
+        self.description = description
+        print("Создана заметка")
 
-task = Task()
-
-task.set_info("Сделать лекцию")
-
-print(task.title)
-
-task.get_info()
-Task.get_info(task) # то как будет обработан вызов выше интерпретатором
+note = Note("Это title", "Это description")
+print(note.title)
+print(note.description)
