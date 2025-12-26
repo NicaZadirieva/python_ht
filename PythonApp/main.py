3d7e79b48e3346b2dfc48dd6a2a8dc729a27ae2e
NicaZadirieva <no.demchenko@yandex.ru>
2025-12-26 20:33:10 +0300

    demo: декоратор property

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,27 +1,12 @@
-class ShoppingList:
-    """Список покупок"""
+class Rectangle:
+    def __init__(self, width: float, height: float):
+        self.width = width
+        self.height = height
 
-    def __init__(self, items: list[str]):
-        self.items: list[str] = items
+    @property
+    def area(self):
+        """Площадь"""
+        return self.width * self.height
 
-    def __eq__(self, value):
-        if not isinstance(value, ShoppingList):
-            return False
-        return self.items == value.items
-
-    def __len__(self):
-        return len(self.items)
-
-    def __getitem__(self, index):
-        return self.items[index]
-
-    def __repr__(self):
-        return f"ShoppingList(items={",".join(self.items)})"
-
-list_1 = ShoppingList(["Яблоко"])
-list_2 = ShoppingList(["Помидор"])
-
-print(list_1 == list_2)
-print(len(list_1))
-print(list_1[0])
-print(list_2)
+rect = Rectangle(10, 5)
+print(rect.area)
