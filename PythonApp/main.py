8480a4f30900519be9c6484ded1c5a002277a1e5
NicaZadirieva <no.demchenko@yandex.ru>
2025-12-27 19:09:09 +0300

    demo: классовый декоратор

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,21 +1,27 @@
 from functools import wraps
 
-def limit_calls(times: int):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(self, *args, **kwargs):
-            current_counter = getattr(self, "count_attr_" + func.__name__, 0)
-            if current_counter >= times:
-                raise RuntimeError("Not valid invoking")
-            setattr(self, "count_attr_" + func.__name__, current_counter + 1)
-            return func(self, *args, **kwargs)
-        return wrapper
-    return decorator
 
+class LimitCalls:
+    def __init__(self, times: int):
+        self.times = times
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapper(self_obj, *args, **kwargs):
+            attr_name = "count_attr_" + func.__name__
+            current_counter = getattr(self_obj, attr_name, 0)
+            
+            if current_counter >= self.times:
+                raise RuntimeError("Not valid invoking")
+            
+            setattr(self_obj, attr_name, current_counter + 1)
+            return func(self_obj, *args, **kwargs)
+        
+        return wrapper
 
 
 class Engine:
-    @limit_calls(3)
+    @LimitCalls(3)
     def start(self):
         print("Двигатель запущен")
 
