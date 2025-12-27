b160b304a5c04d3ad8fb6100796016b03411366b
NicaZadirieva <no.demchenko@yandex.ru>
2025-12-27 19:07:01 +0300

    refactor: упрощение задачи с limit_calls

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -4,16 +4,11 @@ def limit_calls(times: int):
     def decorator(func):
         @wraps(func)
         def wrapper(self, *args, **kwargs):
-            current_counter = 0
-            if not hasattr(self, "count_attr_" + func.__name__):
-                setattr(self, "count_attr_" + func.__name__, current_counter + 1)
-            else:
-                current_counter = getattr(self, "count_attr_" + func.__name__)
-                setattr(self, "count_attr_" + func.__name__, current_counter + 1)
-            if current_counter + 1 >= times:
+            current_counter = getattr(self, "count_attr_" + func.__name__, 0)
+            if current_counter >= times:
                 raise RuntimeError("Not valid invoking")
-            else:
-                return func(self, *args, **kwargs)
+            setattr(self, "count_attr_" + func.__name__, current_counter + 1)
+            return func(self, *args, **kwargs)
         return wrapper
     return decorator
 
