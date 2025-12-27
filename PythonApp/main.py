db7651cea0c04224fa43a016471124e3670f2582
NicaZadirieva <no.demchenko@yandex.ru>
2025-12-27 18:25:51 +0300

    demo: исправление ошибок потери контекста при использовании wrapper

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,4 +1,7 @@
+from functools import wraps
+
 def log_call(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         print(f"Log {func.__qualname__} {args} {kwargs}")
         return func(*args, **kwargs)
@@ -9,4 +12,11 @@ class Service:
     def process(self, x: float) -> float:
         return x * 2
 
-Service().process(5)
+s = Service()
+
+def a():
+    return 1
+
+print(a.__name__)
+
+print(s.process.__name__)
