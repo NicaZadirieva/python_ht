f92b7e264d2f8845d5543c572cb5be0c8bae4547
NicaZadirieva <no.demchenko@yandex.ru>
2025-12-27 18:13:50 +0300

    feat: моя реализация retry (макисмальное количество вызовов функции с ошибкой)

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,13 +1,26 @@
-def repeat(times: int):
+import random
+
+
+def retry(max_times: int):
     def decorator(func):
         def wrapper(*args, **kwargs):
-            for _ in range(times):
+            is_success = False
+            for _ in range(max_times - 1):
+                try:
+                    func(*args, **kwargs)
+                    is_success = True
+                    break
+                except:
+                    pass
+            if not is_success:
                 func(*args, **kwargs)
         return wrapper
     return decorator
 
-@repeat(3)
-def hello():
-    print("hello")
+@retry(3)
+def unstable():
+    if random.random() < 0.7:
+        raise ValueError("Ошибка соединения")
+    print("Успешное соединение")
 
-hello()
+unstable()
