8084aaadad1286be648c7cb0b0b4b2304c926f4d
NicaZadirieva <no.demchenko@yandex.ru>
2025-12-27 19:01:44 +0300

    feat: моя реализация limit_calls (с ошибкой)

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,22 +1,33 @@
 from functools import wraps
 
-def log_call(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        print(f"Log {func.__qualname__} {args} {kwargs}")
-        return func(*args, **kwargs)
-    return wrapper
+def limit_calls(times: int):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            current_counter = 0
+            if not hasattr(self, "count_attr_" + func.__name__):
+                setattr(self, "count_attr_" + func.__name__, current_counter + 1)
+            else:
+                current_counter = getattr(self, "count_attr_" + func.__name__)
+                setattr(self, "count_attr_" + func.__name__, current_counter + 1)
+            if current_counter + 1 >= times:
+                raise RuntimeError("Not valid invoking")
+            else:
+                return func(self, *args, **kwargs)
+        return wrapper
+    return decorator
 
-class Service:
-    @log_call
-    def process(self, x: float) -> float:
-        return x * 2
 
-s = Service()
 
-def a():
-    return 1
+class Engine:
+    @limit_calls(3)
+    def start(self):
+        print("Двигатель запущен")
 
-print(a.__name__)
 
-print(s.process.__name__)
+car = Engine()
+car.start()
+car.start()
+car.start()
+
+car.start() # RuntimeError
