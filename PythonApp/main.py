700a648dee756f383017e6d85fb0636e4f08357f
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-10 17:58:39 +0300

    demo: упражнение на наследование

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,30 +1,43 @@
-from functools import wraps
+class Course:
+    """Базовый класс курса"""
+    def __init__(self, price: int, name: str, duration_ms: int):
+        self.__price = price
+        self.name = name
+        self.duration_ms = duration_ms
+
+    def get_price(self):
+        """Получение цены"""
+        return self.__price
+
+    def print_info(self):
+        """Вывод информации о курсе"""
+        print(f"{self.name}: {self.duration_ms}ms, {self.get_price()}rub")
 
 
-class LimitCalls:
-    def __init__(self, times: int):
-        self.times = times
+class AiCourse(Course):
+    """АИ и тренажеры"""
+    def get_credit(self):
+        """Расчет рассрочки"""
+        credit = self.get_price() / self.duration_ms
+        return credit
 
-    def __call__(self, func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            if self.times <= 0:
-                raise RuntimeError("Not valid invoking")
-            self.times -= 1
-            return func(*args, **kwargs)
-        
-        return wrapper
+class ProjectCourse(Course):
+    """Проектный курс"""
+    def __init__(self, price: int, name: str, duration_ms: int, project_name: str):
+        super().__init__(price, name, duration_ms)
+        self.__project_name = project_name
+
+    def print_info(self):
+        """Вывод информации о курсе"""
+        print(f"{self.name}, {self.__project_name}: {self.duration_ms}ms, {self.get_price()}rub")
+
+    def get_credit(self):
+        """Расчет рассрочки"""
+        credit = self.get_price() / self.duration_ms
+        return credit
+
+    def get_project_info(self):
+        """Информация о проекте"""
+        return self.__project_name
 
 
-class Engine:
-    @LimitCalls(3)
-    def start(self):
-        print("Двигатель запущен")
-
-
-car = Engine()
-car.start()
-car.start()
-car.start()
-
-car.start() # RuntimeError
