8c0824b88c3e00271ff8bbf54dcbf0a45ab5b438
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-12 11:39:05 +0300

    demo: задача со студентами

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -27,5 +27,75 @@
 Печать отчета с помощью созданного метода.
 """
 
+from dataclasses import dataclass
+
+@dataclass
+class Student:
+    """Модель студента"""
+    name: str
+    score: float
+
+class StudentStorage:
+    """Хранение списка студентов"""
+    students: list[Student] = []
+
+    def add_student(self, student: Student) -> Student:
+        """Добавление нового студента"""
+        self.students.append(student)
+        return student
+
+    def get_all_students(self) -> list[Student]:
+        """Получение списка всех студентов"""
+        return self.students
+
+@dataclass
+class StudentStatistics:
+    """Статистика студентов"""
+    studentStorage: StudentStorage
+
+    def get_best_student(self):
+        """Метод для нахождения студента с наилучшей оценкой"""
+        students = self.studentStorage.get_all_students()
+        best_student = students[0]
+        for student in students:
+            if student.score > best_student.score:
+                best_student = student
+        return best_student
 
 
+@dataclass
+class StudentReportService:
+    """Печать отчета"""
+    studentStatistics: StudentStatistics
+    studentStorage: StudentStorage
+
+    def generate_best(self) -> None:
+        best_student = self.studentStatistics.get_best_student()
+        print(f"Best student: {best_student.name} {best_student.score}")
+
+    def generate_all(self) -> None:
+        students = self.studentStorage.get_all_students()
+        for student in students:
+            print(f"Student: {student.name} {student.score}")
+
+# пример использования
+studentStorage = StudentStorage()
+studentStatistics = StudentStatistics(studentStorage)
+studentReportService = StudentReportService(studentStatistics, studentStorage)
+
+studentStorage.add_student(
+    Student(
+        name="Вероника",
+        score=3.5
+    )
+)
+
+studentStorage.add_student(
+    Student(
+        name="Полечка",
+        score=4.5
+    )
+)
+
+studentReportService.generate_all()
+studentReportService.generate_best()
