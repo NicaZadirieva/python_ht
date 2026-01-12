db9422afab2a60d0a2b927fe081dd66ae7b00c44
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-12 12:47:33 +0300

    demo: упражнение на LSP

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -5,32 +5,49 @@
 
 from dataclasses import dataclass
 
+@dataclass
+class Payment():
+    total_amount: int = 0
+
+    def pay(self, amount: int):
+        if amount > self.total_amount:
+            raise ValueError("Значение amount > суммы в кошельке")
+        self.total_amount -= amount
+        return self.total_amount
+
 
 @dataclass
-class User:
-    name: str
-    bonus: int = 0
+class BonusPayment(Payment):
+    bonuses: int = 0
+    def pay(self, amount: int):
+        if amount + self.bonuses > self.total_amount:
+            raise ValueError("Значение amount + bonuses > суммы в кошельке")
+        self.total_amount -= amount + self.bonuses
+        return self.total_amount
 
-    def add_bonus(self, amount: int):
-        self.bonus += amount
-        print(f"Пользователь {self.name} получил {amount} бонусов. Всего {self.bonus} бонусов")
+
+@dataclass
+class InstallmentPayment(Payment):
+    n: int = 1
+    def pay(self, amount: int):
+        if self.n == 0:
+            print("Рассрочка оплачена")
+            return self.total_amount
+
+        amount_with_n = amount / self.n
+        if amount_with_n > self.total_amount:
+            raise ValueError("Значение amount > суммы в кошельке")
+
+        self.total_amount -= amount_with_n
+        self.n -= 1
+
+        return self.total_amount
 
 
-class PremiumUser(User):
-    def add_bonus(self, amount: int):
-        self.bonus += amount * 2
-        print(f"Пользователь {self.name} получил {amount * 2} бонусов. Всего {self.bonus} бонусов")
+payment = Payment(100)
+bonusPayment = BonusPayment(100, 11)
+installmentPayment = InstallmentPayment(100, 10)
 
-# нарушение LSP.
-class BannedUser(User):
-    def add_bonus(self, amount: int):
-        print(f"Пользователь {self.name} не может получить бонусы")
-
-user = User("Вася")
-
-def reward_user(user: User):
-    user.add_bonus(100)
-
-reward_user(user)
-reward_user(PremiumUser("Вася2"))
-reward_user(BannedUser("Вася3"))
+print(f"Simple payment: {payment.pay(99)}")
+print(f"Bonus payment: {bonusPayment.pay(1)}")
+print(f"Install payment: {installmentPayment.pay(99)}")
