e2df59c355f581344324c4ad9c92541bd981fce2
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-12 14:09:19 +0300

    chore: упражнение на DIP

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -5,25 +5,8 @@
 Оба типа модулей должны зависеть от абстракций, а не абстракции от деталей.
 """
 
-from dataclasses import dataclass
-from abc import ABC, abstractmethod
-
-class Logger(ABC):
-    @abstractmethod
-    def log(self, message: str):
-       pass
-
-class FileLogger(Logger):
-    def log(self, message: str):
-        print(f"Запись в файл: {message}")
-
-class ConsoleLogger(Logger):
-    def log(self, message: str):
-        print(f"Запись в консоль: {message}")
-
-@dataclass
-class UserService:
-    logger: Logger
-
-    def create_user(self, name: str):
-        self.logger.log(f"Создан аккаунт {name}")
+"""
+Сделать LowStockService который в методе run проверяет в 
+InMemoryStockRepository - сколько осталось товара (число items)
+и если их меньше 10 - отправляет уведомление через EmailNotifier
+"""
