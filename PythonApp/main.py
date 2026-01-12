8450b46b25e1650dcc879651e1304320044138be
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-12 11:02:11 +0300

    demo: пример несоответствия к SRP

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,51 +1,19 @@
-"""Создать два типа хранилищ данных: в памяти (memory storage) и в файле (file storage)."""
-from abc import ABC, abstractmethod
+"""Single Responsibility Principle"""
+""""Класс должен иметь только одну причину для изменения, то есть он должен выполнять лишь одну функцию."""
+
 from dataclasses import dataclass
 
-class Storage(ABC):
-    """Протокол для хранилища данных"""
-
-    @abstractmethod
-    def save(self, data: str) -> None: ...
-
-    @abstractmethod
-    def load(self) -> str: ...
-
-    def log(self):
-        print("Сохранено")
-
 @dataclass
-class MemoryStorage(Storage):
-    """Хранит данные в памяти"""
-    storage_data: str = ""
-    def save(self, data: str) -> None:
-        self.storage_data = data
+class Order:
+    items: list[str]
 
-    def load(self) -> str:
-        return self.storage_data
+    def calculate_total(self):
+        return len(self.items) * 10
 
+    # не относится к Order
+    def save_to_db(self):
+        print("Сохранение в базу")
 
-
-@dataclass
-class FileStorage(Storage):
-    """Хранит данные в файле"""
-    file_name: str
-    def save(self, data: str) -> None:
-        with open(self.file_name, 'w', encoding="utf-8") as file:
-            file.write(data)
-
-    def load(self) -> str:
-        with open(self.file_name, 'r', encoding="utf-8") as file:
-            return file.read()
-
-
-def use_storage(storage: Storage, data: str):
-    storage.save(data)
-    return storage.load()
-
-mem = MemoryStorage()
-file = FileStorage("file.txt")
-
-user_input = input("Введите данные: ")
-print(use_storage(mem, user_input))
-print(use_storage(file, user_input))
+    # не относится к Order
+    def send_confirmation_email(self):
+        print("Отправка письма")
