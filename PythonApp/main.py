59e63a4fd9234beb29106fbdabe288e94535c90e
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-12 12:02:42 +0300

    demo: нарушение OCP

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,118 +1,12 @@
-"""Single Responsibility Principle"""
-""""Класс должен иметь только одну причину для изменения, то есть он должен выполнять лишь одну функцию."""
-"""
-Упражнение заключается в следующем:
+"""Принцип открытости-закрытости"""
+"""Классы должны быть открыты для расширения, но закрыты для модификации."""
 
-Создание модели студента:
-
-Студент имеет имя и оценку.
-Реализация через data-класс.
-Создание класса репозитория студентов:
-
-Хранение списка студентов.
-Методы для добавления нового студента и получения списка всех студентов.
-Создание класса для статистики студентов:
-
-Метод для расчета средней оценки.
-Метод для нахождения студента с наилучшей оценкой.
-Взаимодействие с репозиторием для получения данных.
-Создание класса для печати отчета:
-
-Генерация отчета с информацией о студенте, среднем балле и лучшем студенте.
-Вывод всех студентов и их оценок.
-Практическая реализация:
-
-Создание экземпляров каждого из классов.
-Добавление нескольких студентов в репозиторий.
-Печать отчета с помощью созданного метода.
-"""
-
-from dataclasses import dataclass, field
-
-@dataclass
-class Student:
-    """Модель студента"""
-    name: str
-    score: float
-
-@dataclass
-class StudentStorage:
-    """Хранение списка студентов"""
-    students: list[Student] = field(default_factory=list)
-
-    def add_student(self, student: Student) -> Student:
-        """Добавление нового студента"""
-        self.students.append(student)
-        return student
-
-    def get_all_students(self) -> list[Student]:
-        """Получение списка всех студентов"""
-        return self.students
-
-@dataclass
-class StudentStatistics:
-    """Статистика студентов"""
-    studentStorage: StudentStorage
-
-    def get_best_student(self):
-        """Метод для нахождения студента с наилучшей оценкой"""
-        students = self.studentStorage.get_all_students()
-        best_student = max(students, key=lambda s: s.score)
-        return best_student
-
-    def get_average_score(self):
-        """Метод для нахождения среднего балла"""
-        students = self.studentStorage.get_all_students()
-        sum_score = 0
-        for student in students:
-            sum_score += student.score
-        return sum_score / len(students) if len(students) > 0 else 0 
-
-
-@dataclass
-class StudentReportPrinter:
-    """Печать отчета"""
-    studentStatistics: StudentStatistics
-    studentStorage: StudentStorage
-
-    def __generate_best__(self) -> None:
-        """Печать лучшего студента"""
-        best_student = self.studentStatistics.get_best_student()
-        print(f"Best student: {best_student.name} {best_student.score}")
-
-    def __generate_all__(self) -> None:
-        """Печать всех студентов"""
-        students = self.studentStorage.get_all_students()
-        for student in students:
-            print(f"Student: {student.name} {student.score}")
-
-    def __generate_average_score__(self) -> None:
-        """Печать средней оценки всех студентов"""
-        print(f"Average score: {self.studentStatistics.get_average_score()}")
-
-    def print_report(self) -> None:
-        """Печать отчета"""
-        self.__generate_all__()
-        self.__generate_best__()
-        self.__generate_average_score__()
-
-# пример использования
-studentStorage = StudentStorage()
-studentStatistics = StudentStatistics(studentStorage)
-studentReportPrinter = StudentReportPrinter(studentStatistics, studentStorage)
-
-studentStorage.add_student(
-    Student(
-        name="Вероника",
-        score=3.5
-    )
-)
-
-studentStorage.add_student(
-    Student(
-        name="Полечка",
-        score=4.5
-    )
-)
-
-studentReportPrinter.print_report()
+class DiscountCalculator:
+    """Нарушение принципа, так как может измениться логика user_type"""
+    def calculate(self, user_type: str, amount: float) -> float:
+        if user_type == "student":
+            return amount * 0.9
+        elif user_type == "vip":
+            return amount * 0.8
+        else:
+            return amount
