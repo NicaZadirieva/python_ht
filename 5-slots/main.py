5b68353fe0a7400659e19cf1b806fe12a59b7721
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-20 13:04:31 +0300

    feat: создание классов SlotUser и User

--- 5-slots/main.py
+++ 5-slots/main.py
@@ -1,1 +1,18 @@
+﻿from dataclasses import dataclass
+
+
+
+@dataclass
+class User:
+    name: str
+    email: str
+    password: str
+
+@dataclass
+class SlotUser:
+    __slots__=('name', 'email', 'password')
+    name: str
+    email: str
+    password: str
+
 
