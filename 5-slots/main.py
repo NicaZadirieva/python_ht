3340860624ef8326b70f6b4e4aba39f78a45f1c9
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-20 13:07:46 +0300

    feat: генерация списков

--- 5-slots/main.py
+++ 5-slots/main.py
@@ -15,4 +15,25 @@ class SlotUser:
     email: str
     password: str
 
+# Создаем списки
+users_list = []
+slot_users_list = []
+
+for i in range(100000):
+    # Создаем User
+    user = User(
+        name=f"User_{i}",
+        email=f"user_{i}@example.com",
+        password=f"password_{i}"
+    )
+    users_list.append(user)
+    
+    # Создаем SlotUser с теми же данными
+    slot_user = SlotUser(
+        name=f"User_{i}",
+        email=f"user_{i}@example.com",
+        password=f"password_{i}"
+    )
+    slot_users_list.append(slot_user)
+
 
