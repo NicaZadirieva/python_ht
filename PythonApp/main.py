74b0f0d1514b1cdbc7deed79671f0b78c051a04a
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-21 11:22:31 +0300

    demo: typevar

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,1 +1,13 @@
-﻿
+﻿from typing import TypeVar
+
+T = TypeVar("T")
+
+def first_item(items: list[T]) -> T:
+    return items[0]
+
+
+users = ["Anton", "Vasya"]
+num = [1, 2, 3]
+
+res = first_item(users)
+    
