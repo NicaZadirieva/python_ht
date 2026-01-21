c98dfcf324ba2326f5e6614f97db7a4749d3c36d
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-21 11:56:35 +0300

    demo: упражнение с optional

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,23 +1,38 @@
-﻿from dataclasses import dataclass
-from typing import Optional
+﻿"""
+save_get:
+Цель: Получение значения из списка по индексу с безопасной обработкой.
+Описание: Функция принимает список (list[T]) и индекс (int).
+Возврат: Возвращает элемент списка (T) или None, если индекс за пределами списка.
+Используемые методы: Optional[T] для возврата либо значения, либо None.
+map_optional:
+Цель: Применение функции к значению, если оно не None.
+Описание: Функция принимает значение (Optional[T]) и трансформер- функцию (Callable[[T], R]).
+Возврат: Возвращает преобразованное значение (Optional[R]), либо None, если исходное значение было None.
+Используемые методы: Optional, Callable, два дженерика для типизации входного и выходного типов.
+or_else:
+Цель: Возврат значения, если оно не None, или дефолтного значения в противном случае.
+Описание: Функция принимает значение (Optional[T]) и дефолтное значение (T).
+Возврат: Возвращает значение (T), либо дефолтное, если основное значение None.
+"""
+from typing import TypeVar, Optional, Callable
 
+T = TypeVar("T")
+R = TypeVar("R")
 
-@dataclass
-class User:
-    id: int
-    name: str
-    email: str
+def save_get(arr: list[T], i: int) -> Optional[T]:
+    return None if len(arr) <= i else arr[i]
 
-def get_user_by_id(user_id: int) -> Optional[User]:
-    users = [
-        User(1, "Anton", "a@a.ru"),
-        User(2, "Vasya", "v@v.ru"),
-    ]
+def map_optional(a: Optional[T], callback: Callable[[T], R]) -> Optional[R]:
+    if a == None:
+        return None
+    return callback(a)
 
-    for user in users:
-        if user.id == user_id:
-            return user
+def or_else(a: Optional[T], default: T) -> T:
+    return default if a is None else a
 
-    return None
+arr = [1, 2, 3]
+print(save_get(arr, 4))
 
-get_user_by_id(999)
+print([map_optional(a, str) for a in arr])
+
+print(or_else(save_get(arr, 4), "default"))
