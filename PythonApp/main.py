f17f9495764518ba5ea0501e8ee53989a1a4020c
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-21 13:59:33 +0300

    demo: type guard

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,9 +1,14 @@
-﻿def safe_div(a: float, b: float) -> float | str:
-    if b == 0:
-        return "деление на ноль"
-    return a / b
+﻿from typing import TypeGuard
 
-def ensure_list(value: str | list[str]) -> list[str]:
-    if isinstance(value, list):
-        return value
-    return [value]
+
+def is_int_list(x: list[int] | list[str]) -> TypeGuard[list[int]]:
+    return all(isinstance(i, int) for i in x)
+
+def is_str_list(x: list[int] | list[str]) -> TypeGuard[list[str]]:
+    return all(isinstance(i, str) for i in x)
+
+def f(xs: list[int] | list[str]):
+    if is_int_list(xs):
+        xs[0].is_integer()
+    if is_str_list(xs):
+        xs[0].capitalize()
