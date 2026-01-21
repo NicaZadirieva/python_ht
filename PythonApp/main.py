11a64574576259c719d28d6b8e2b9f531f28fbfb
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-21 12:59:32 +0300

    demo: union

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,1 +1,9 @@
-﻿
+﻿def safe_div(a: float, b: float) -> float | str:
+    if b == 0:
+        return "деление на ноль"
+    return a / b
+
+def ensure_list(value: str | list[str]) -> list[str]:
+    if isinstance(value, list):
+        return value
+    return [value]
