bd15c7e51886362c81102593de133c9b0c5b1bd6
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-23 23:03:27 +0300

    demo: wait_for

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,12 +1,15 @@
 ﻿import asyncio
 import aiohttp
 
+async def fetch(session, url):
+    return await asyncio.wait_for(session.get(url), timeout=2)
+
 # получение в задачах конкуретно 10 раз обращение к гугл
 async def main():
     async with aiohttp.ClientSession() as session:
         urls = ["https://google.com"] * 10
         tasks = [
-            asyncio.create_task(session.get(url, timeout=10)) for url in urls
+            asyncio.create_task(fetch(session, url)) for url in urls
         ]
         done, _ = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
         results = [task.result() for task in done]
