188b158436d6da11a0c81879e63f868582e041f8
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-23 22:50:27 +0300

    demo: решение лектора

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -5,8 +5,9 @@ import aiohttp
 # получение в задачах конкуретно 10 раз обращение к гугл
 async def main():
     async with aiohttp.ClientSession() as session:
-        tasks = [session.get("https://google.com", timeout=10) for _ in range(10)]
+        urls = ["https://google.com"] * 10
+        tasks = [session.get(url, timeout=10) for url in urls]
         results = await asyncio.gather(*tasks)
-        print(",".join([str(r.status) for r in results]))
+        print(list(map(lambda x: x.status, results)))
 
 asyncio.run(main())
