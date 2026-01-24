4c83f424a91bb9ed6a69583bd86c2d0b6cef1464
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-24 20:06:29 +0300

    feat: пример с add_done_callback

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,18 +1,19 @@
 ﻿import asyncio
 import aiohttp
 
-async def fetch(session, url):
-    return await asyncio.wait_for(session.get(url), timeout=2)
+async def bad():
+    print("Начата")
+    raise ValueError("Error")
 
 # получение в задачах конкуретно 10 раз обращение к гугл
 async def main():
-    async with aiohttp.ClientSession() as session:
-        urls = ["https://google.com"] * 10
-        tasks = [
-            asyncio.create_task(fetch(session, url)) for url in urls
-        ]
-        done, _ = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-        results = [task.result() for task in done]
-        print(list(map(lambda x: x.status, results)))
+    try:
+        task = asyncio.create_task(bad())
+        task.add_done_callback(lambda t: print("Ошибка", t.exception()))
+        await asyncio.sleep(2)
+        print("Ждем")
+        await task
+    except ValueError as e:
+        print(e)
 
 asyncio.run(main())
