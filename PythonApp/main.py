0c493475e704211dc3497460a083cf06a4352b4e
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-24 21:06:25 +0300

    feat: моя реализация упражнения run_with_retry

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -7,10 +7,22 @@ async def unstable():
         raise ValueError("Случайная ошибка")
     return "OK"
 
+async def run_with_retry(task, max_count=5):
+    for _ in range(max_count - 1):
+        try:
+            res = await task
+            return res
+        except:
+            pass
+    res = await task
+    return res
+    
+
 async def main():
     # run_with_retry - сделать корутину, которая запустит корутину
     # если она выбросила ошибку, пробует заново до указанного лимита
-    pass
+    res = await run_with_retry(asyncio.create_task(unstable()))
+    print(res)
 
 
 asyncio.run(main())
