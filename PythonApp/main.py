1df0ec64bb099f8c9e0f3519d04259973ef436d6
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-24 20:39:59 +0300

    использование shield

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,15 +1,21 @@
 ﻿import asyncio
 import aiohttp
 
+async def save():
+    print("Сохраняю")
+    await asyncio.sleep(5)
+    print("Сохранено")
+
 async def job():
     print("Работаю")
+    t = asyncio.create_task(save())
+    await asyncio.shield(t)
     await asyncio.sleep(5)
-    print("Готово!")
-
+    print("Готово")
 
 async def main():
     task = asyncio.create_task(job())
-    await asyncio.sleep(1)
+    await asyncio.sleep(5)
     task.cancel()
     try:
         await task
