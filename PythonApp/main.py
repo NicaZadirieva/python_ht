519fa74c4b5d3f29d27d282f352609ccc3cf1d1c
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-24 20:55:35 +0300

    chore: условие задачи run_with_retry

--- PythonApp/main.py
+++ PythonApp/main.py
@@ -1,28 +1,16 @@
 ﻿import asyncio
-import aiohttp
+import random
 
-async def save():
-    print("Сохраняю")
-    await asyncio.sleep(5)
-    print("Сохранено")
-
-async def job():
-    print("Работаю")
-    t = asyncio.create_task(save())
-    await asyncio.shield(t)
-    await asyncio.sleep(5)
-    print("Готово")
+async def unstable():
+    await asyncio.sleep(0.2)
+    if random.random() < 0.5:
+        raise ValueError("Случайная ошибка")
+    return "OK"
 
 async def main():
-    task = asyncio.create_task(job())
-    await asyncio.sleep(5)
-    task.cancel()
-    try:
-        await task
-    except asyncio.CancelledError:
-        print(task.cancelled())
-        print("Задача отменена")
-
+    # run_with_retry - сделать корутину, которая запустит корутину
+    # если она выбросила ошибку, пробует заново до указанного лимита
+    pass
 
 
 asyncio.run(main())
