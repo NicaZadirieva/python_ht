b5f5db732542d37ea01784ceb60fedd5e4422c70
NicaZadirieva <no.demchenko@yandex.ru>
2026-01-28 15:52:42 +0300

    feat: добавила кнопку Выход

--- PythonApp/note_app/screens/main.py
+++ PythonApp/note_app/screens/main.py
@@ -4,7 +4,7 @@
 
 from textual.screen import Screen
 from textual.app import ComposeResult
-from textual.widgets import Header
+from textual.widgets import Header, Footer
 
 
 class MainScreen(Screen):
@@ -12,11 +12,20 @@ class MainScreen(Screen):
     Класс для главного экрана
     """
 
+    BINDINGS = [("q", "quit", "Выход")]
+
     def compose(self) -> ComposeResult:
         yield Header()
+        yield Footer()
 
     def on_mount(self):
         """
         Монтирование
         """
         self.title = "Менеджер заметок"
+
+    def action_quit(self):
+        """
+        Выход из приложения
+        """
+        self.app.exit()
