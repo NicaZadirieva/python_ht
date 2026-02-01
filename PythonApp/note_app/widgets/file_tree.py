88dce733b35f3907e23f1532789c8608d4af198d
NicaZadirieva <no.demchenko@yandex.ru>
2026-02-01 18:21:39 +0300

    feat: FileTree expand

--- PythonApp/note_app/widgets/file_tree.py
+++ PythonApp/note_app/widgets/file_tree.py
@@ -1,8 +1,11 @@
+from pathlib import Path
 from textual.containers import VerticalScroll
 from textual.app import ComposeResult
-from textual.widget import Widget
+from textual.events import Mount
 from textual.widgets import Tree
+from textual.widgets._tree import TreeNode
 
+from note_app.domain.folder import Folder
 from note_app.repositories import BaseFolderRepository
 
 
@@ -21,3 +24,17 @@ class FileTreeWidget(VerticalScroll):
     def compose(self) -> ComposeResult:
         self._tree = Tree(label="Заметки")
         yield self._tree
+
+    def _on_mount(self, event: Mount) -> None:
+        root = self._tree.root
+        root.data = Folder("data", Path("data"))
+        root.expand()
+
+    def on_tree_node_expanded(self, event: Tree.NodeExpanded) -> None:
+        node: TreeNode[Folder] = event.node
+        node.remove_children()
+        path = node.data.path if node.data else ""
+        folders = self._folder_repo.get_folders_by_path(Path(path))
+
+        for folder in folders:
+            node.add(folder.name, folder)
