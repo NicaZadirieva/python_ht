f1c1afd1f23d6f8cd15fe70ada327a580d84dcf2
NicaZadirieva <no.demchenko@yandex.ru>
2026-02-01 14:40:40 +0300

    feat: Поле ввода ссылки и интервала

--- 8-final/textual_app/screens/main.py
+++ 8-final/textual_app/screens/main.py
@@ -5,7 +5,8 @@
 from textual.events import Mount
 from textual.screen import Screen
 from textual.app import ComposeResult
-from textual.widgets import Header, Footer
+from textual.widgets import Header, Footer, Button, Input, Label
+from textual.containers import Horizontal, Vertical
 
 
 class MainScreen(Screen):
@@ -13,10 +14,36 @@ class MainScreen(Screen):
     Класс для основного экрана
     """
 
+    CSS = """
+    .form-input {
+        width: 100;
+    }
+    .label {
+        padding: 1;
+    }
+    .button-add {
+        margin-left: 1
+    }
+    """
     BINDINGS = [("q", "quit", "Выход")]
 
     def compose(self) -> ComposeResult:
         yield Header()
+
+        yield Horizontal(
+            Horizontal(
+                Label("URL", classes="label"),
+                Input(placeholder="URL..."),
+                classes="form-input",
+            ),
+            Horizontal(
+                Label("Interval", classes="label"),
+                Input(placeholder="interval..."),
+                classes="form-input",
+            ),
+            Button("ADD", classes="button-add"),
+        )
+
         yield Footer()
 
     def _on_mount(self, event: Mount) -> None:
