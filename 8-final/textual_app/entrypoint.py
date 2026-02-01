1670187353df779b4771e27eac84438dc6f40618
NicaZadirieva <no.demchenko@yandex.ru>
2026-02-01 13:52:38 +0300

    docs: add docs comments

--- 8-final/textual_app/entrypoint.py
+++ 8-final/textual_app/entrypoint.py
@@ -1,7 +1,18 @@
+"""
+Запуск приложения
+"""
+
 import asyncio
 
+
 async def main():
-	pass
+    """
+    Точка входа в приложение
+    """
+
 
 def run():
-	asyncio.run(main())
+    """
+    Функция запуска приложения
+    """
+    asyncio.run(main())
