da0d887a3b705c54a51eb677a60ef8b8d15e757f
NicaZadirieva <no.demchenko@yandex.ru>
2026-02-02 11:50:54 +0300

    feat: обработка нажатий кнопок в модалке

--- PythonApp/note_app/screens/import_modal.py
+++ PythonApp/note_app/screens/import_modal.py
@@ -7,7 +7,7 @@ from textual.containers import Container, Horizontal
 from textual.widgets import Static, Input, Button
 
 
-class ImportModal(ModalScreen):
+class ImportModal(ModalScreen[str]):
     """
     Класс модального окна для импорта
     """
@@ -35,13 +35,27 @@ class ImportModal(ModalScreen):
     def compose(self):
         with Container(id="dialog"):
             yield Static("Импорт данных", id="title")
-            yield Input(placeholder="Введите url для импорта")
+            yield Input(placeholder="Введите url для импорта", id="input_url")
             with Horizontal(id="buttons"):
-                yield Button("Импортировать", variant="primary")
-                yield Button("Отмена")
+                yield Button("Импортировать", variant="primary", id="import_btn")
+                yield Button("Отмена", id="cancel_btn")
 
     def key_escape(self) -> None:
         """
         Закрытие по нажатию на кнопку "Esc"
         """
         self.dismiss()
+
+    def on_button_pressed(self, event: Button.Pressed):
+        if event.button.id == "import_btn":
+            url_input = self.query_one("#input_url", Input)
+            url = url_input.value.strip()
+            if url:
+                pass
+            else:
+                url_input.styles.border = ("solid", "red")
+        else:
+            self.dismiss(None)
+
+    async def import_data(self, url: str) -> None:
+        pass
